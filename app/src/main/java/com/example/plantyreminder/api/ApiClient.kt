1a50e222bede4b84d5d213bc1a3e37a3a71fb582
Jakub Piasecki <jakubpiasecki05@gmail.com>
2023-04-01 14:25:26 +0200

    Added simple error handling and unit tests

--- app/src/main/java/com/example/plantyreminder/api/ApiClient.kt
+++ app/src/main/java/com/example/plantyreminder/api/ApiClient.kt
@@ -1,10 +1,12 @@
 package com.example.plantyreminder.api
 
+import com.example.plantyreminder.api.dto.ApiPlantObject
 import com.example.plantyreminder.api.dto.ApiPlantObjectList
 import com.example.plantyreminder.data.models.Plant
 import com.example.plantyreminder.utils.toPlant
 import okhttp3.OkHttpClient
 import retrofit2.Converter
+import retrofit2.HttpException
 import retrofit2.Retrofit
 
 class ApiClient(
@@ -25,12 +27,18 @@ class ApiClient(
         apiInterface = retrofit.create(ApiInterface::class.java)
     }
 
-    suspend fun getAll(predicate: String=""): List<Plant> {
-        val res: ApiPlantObjectList = apiInterface.getAll(predicate);
+    suspend fun getAll(predicate: String = ""): Result<List<Plant>> {
+        val data: ApiPlantObjectList?
 
-        return res.results.map {
-            it.toPlant();
+        try {
+            data = apiInterface.getAll(predicate)
+        } catch (e: HttpException) {
+            return Result.failure(e)
         }
+
+        return Result.success(data.results.map {
+            it.toPlant();
+        })
     }
 
 }
--- app/src/test/java/com/example/plantyreminder/ApiClientUnitTest.kt
+++ app/src/test/java/com/example/plantyreminder/ApiClientUnitTest.kt
@@ -4,32 +4,52 @@ import com.example.plantyreminder.api.ApiClient
 import com.example.plantyreminder.api.ApiClientFactory
 import com.example.plantyreminder.data.models.Plant
 import kotlinx.coroutines.runBlocking
-import okhttp3.ResponseBody
-import org.junit.Test
 
-import org.junit.Assert.*
-import retrofit2.Response
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import retrofit2.HttpException
+import java.util.UUID
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ApiClientUnitTest(
+class ApiClientUnitTest {
     private val apiClientFactory: ApiClientFactory = ApiClientFactory()
-) {
+
     @Test
-    fun getAll_isCorrectResponse() = runBlocking {
+    fun `getAll is Correct Response`(): Unit = runBlocking {
 
         val apiClient: ApiClient = apiClientFactory.createGsonApiClient()
         val response = apiClient.getAll()
 
-        assertNotEquals(0, response.size)
-        assertTrue(response.first()::class.java == Plant::class.java)
+        response.onSuccess {
+            assertNotEquals(0, it.size) {"Size of the list is 0"}
+            assertTrue(it.first()::class.java == Plant::class.java) {"Elements are not of type Plant"}
+        }.onFailure {
+            assertEquals(Exception::class.java, it::class.java) { "Returned value is not HttpException" }
+        }
     }
 
-    fun getAll_NotFoundException() = runBlocking {
+    @Test
+    fun `getAll Not Found Exception`(): Unit = runBlocking {
         val apiClient = apiClientFactory.createHttpErrorApiClient()
-        val response = apiClient.getAll();
+
+        apiClient.getAll().onFailure {
+            assertEquals(HttpException::class.java, it::class.java) { "Returned value is not HttpException" }
+        }
+
+    }
+
+    @Test
+    fun `Get Empty List`(): Unit = runBlocking {
+        val apiClient: ApiClient = apiClientFactory.createGsonApiClient()
+        val response = apiClient.getAll(UUID.randomUUID().toString())
+
+        response.onSuccess {
+            assertTrue(it.isEmpty())
+        }
     }
 }
